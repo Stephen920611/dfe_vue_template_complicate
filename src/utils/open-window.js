ef9fec8ff26429ad0ef22ee2f772d7f1c2b40033
stephen <13105208266@163.com>
2020-03-05 13:55:01 +0800

    格式化

--- src/utils/open-window.js
+++ src/utils/open-window.js
@@ -1,25 +1,29 @@
 /**
- *Created by PanJiaChen on 16/11/29.
+ * 打开一个新窗口
+ * Created by PanJiaChen on 16/11/29.
+ * Edited by StephenWang on 2020/03/05
  * @param {Sting} url
  * @param {Sting} title
  * @param {Number} w
  * @param {Number} h
  */
 export default function openWindow(url, title, w, h) {
-  // Fixes dual-screen position                            Most browsers       Firefox
-  const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left
-  const dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top
+    // Fixes dual-screen position                            Most browsers       Firefox
+    const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left
+    const dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top
 
-  const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width
-  const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height
+    //innerwidth 返回窗口的文档显示区的宽度。
+    //document.documentElement.clientWidth  获取浏览器窗口文档显示区域的宽度，不包括滚动条。
+    const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width
+    const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height
 
-  const left = ((width / 2) - (w / 2)) + dualScreenLeft
-  const top = ((height / 2) - (h / 2)) + dualScreenTop
-  const newWindow = window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=yes, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left)
+    const left = ((width / 2) - (w / 2)) + dualScreenLeft
+    const top = ((height / 2) - (h / 2)) + dualScreenTop
+    const newWindow = window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=yes, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left)
 
-  // Puts focus on the newWindow
-  if (window.focus) {
-    newWindow.focus()
-  }
+    // Puts focus on the newWindow
+    if (window.focus) {
+        newWindow.focus()
+    }
 }
 
