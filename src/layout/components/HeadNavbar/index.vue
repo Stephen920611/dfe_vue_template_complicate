535ffce5f81daf1fa58eee94b7f3128e82d4701c
Qn <qina6050@163.com>
2020-03-17 10:53:10 +0800

    添加悬浮小手

--- src/layout/components/HeadNavbar/index.vue
+++ src/layout/components/HeadNavbar/index.vue
@@ -176,6 +176,9 @@
             color: #fff;
             & > i {
                 font-size: 20px;
+                &:hover{
+                    cursor: pointer;
+                }
             }
         }
         .head-nav-bar {
