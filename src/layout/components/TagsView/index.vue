788e9ddd7d4c73009800389e407550afcd94c2a3
Qn <qina6050@163.com>
2020-03-24 09:43:49 +0800

    修改样式

--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -220,7 +220,7 @@ export default {
           const menuMinWidth = 105
           const offsetLeft = this.$el.getBoundingClientRect().left // container margin left
           const offsetWidth = this.$el.offsetWidth // container width
-          const maxLeft = offsetWidth - menuMinWidth // left boundary
+          const maxLeft = offsetWidth - menuMinWidth + 15 // left boundary
           const left = e.clientX - offsetLeft + 15 // 15: margin right
           if (left > maxLeft) {
               this.left = maxLeft
