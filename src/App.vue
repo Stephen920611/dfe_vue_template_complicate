71daf3b86ac4854953efe1f6362e36f4c4a7d011
stephen <13105208266@163.com>
2020-04-25 00:19:23 +0800

    调整出第一个的样式

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+    <div id="app">
+        <router-view/>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  mounted() {
-    //            console.log(11);
-    //            let a = [1,2,3,4,5,6];
-    //            let isProblem = 0;
-    //            a.map(val => {
-    //                console.log(val,'val');
-    //                //0为否，只要有否，isProblem就是1
-    //                if (val === 1) {
-    //                    isProblem = 1;
-    //                    return
-    //                }else {
-    //                    isProblem = 0
-    //                }
-    //            })
-    //            console.log(isProblem,'isProblem');
-  }
-}
+    export default {
+        name: 'App',
+        mounted() {
+
+        }
+    }
 </script>
